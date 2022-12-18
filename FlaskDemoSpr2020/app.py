e1b3cab3639d4000272fa7379fc5c7e39fd8946a
Bharath <39578163+therealbappi@users.noreply.github.com>
2022-12-18 16:37:28 -0500

    Delete FlaskDemoSpr2020 directory

--- FlaskDemoSpr2020/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-#UPLOAD_FOLDER = '/Users/domaghale_1/Downloads/FlaskDemoSpr2020/FlaskDemoPhotos'
-UPLOAD_FOLDER = 'FlaskDemoPhotos'
-
-app = Flask(__name__)
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
